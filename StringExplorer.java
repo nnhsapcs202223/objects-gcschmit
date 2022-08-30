dcf6ee5e67057d075a999cd8ff56a0677953b664
Geoff Schmit <gcschmit@gmail.com>
2022-08-30 14:19:45 -0500

    explore immutable strings

--- StringExplorer.java
+++ StringExplorer.java
@@ -9,6 +9,22 @@ public class StringExplorer
 {
     public static void main(String[] args)
     {
+        String river = new String("Mississippi");
+        System.out.println(river);
+        
+        river.replace("i", "x");
+        System.out.println(river);
+        
+        /*
+         * The replace method returns a reference to a new String object.
+         *  It does *not* change the String object on which it is invoked.
+         *  The String class has no mutator methods. It is not
+         *      possible to change a String object.
+         *      
+         *  Strings are immutable.
+         */
+        String riverX = river.replace("i", "x");
+        System.out.println(riverX);
         
     }
 }
