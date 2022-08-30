71f354faba4de64e1b3650d79c0e8354f86cae38
Geoff Schmit <gcschmit@gmail.com>
2022-08-30 15:28:08 -0500

    intro rectangle 1 activity

---
+++ RectanglePrinter.java
@@ -0,0 +1,13 @@
+import java.awt.Rectangle;
+
+public class RectanglePrinter
+{
+   public static void main(String[] args)
+   {
+      Rectangle r1 = new Rectangle(0, 0, 100, 50);
+      Rectangle r2 = new Rectangle(0, 0, 100, 50);
+      /* Your code goes here */
+      System.out.println(r1);
+      /* and here */
+   }
+}
