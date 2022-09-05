29add391d5bb0d45eec8b415fe053bb907106d31
Geoff Schmit <gcschmit@gmail.com>
2022-09-05 09:37:38 -0500

    add Rectangle 1 sample solutions

--- RectanglePrinter.java
+++ RectanglePrinter.java
@@ -2,12 +2,15 @@ import java.awt.Rectangle;
 
 public class RectanglePrinter
 {
-   public static void main(String[] args)
-   {
-      Rectangle r1 = new Rectangle(0, 0, 100, 50);
-      Rectangle r2 = new Rectangle(0, 0, 100, 50);
-      /* Your code goes here */
-      System.out.println(r1);
-      /* and here */
-   }
+    public static void main(String[] args)
+    {
+        Rectangle r1 = new Rectangle(0, 0, 100, 50);
+        //Rectangle r2 = new Rectangle(0, 0, 100, 50);
+        Rectangle r2 = r1;
+        r2.grow(10, 20);
+
+        System.out.println(r1);
+        System.out.println(r2);
+
+    }
 }
